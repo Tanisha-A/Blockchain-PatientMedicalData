2ad71160a4855ac1d058ec8d1dd4d17cb294f3ff
Tanisha-A <tanisha.admane@gmail.com>
2024-04-06 19:25:48 +0530

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+const { version } = require("react");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.0",
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  }
+};
